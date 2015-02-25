2310b4e4065df4e1ba20cdb83bce65a2bff2c05a
root <root@debian>
2015-02-24 22:51:49 -0300

    first

---
+++ config/initializers/assets.rb
@@ -0,0 +1,21 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( glyphicons-halflings.png )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-white.png )
+Rails.application.config.assets.precompile += %w( Flat-UI-Icons-16.woff )
+Rails.application.config.assets.precompile += %w( Flat-UI-Icons-16.eot )
+Rails.application.config.assets.precompile += %w( Flat-UI-Icons-16.ttf )
+Rails.application.config.assets.precompile += %w( Flat-UI-Icons-24.eot )
+Rails.application.config.assets.precompile += %w( Flat-UI-Icons-24.woff )
+Rails.application.config.assets.precompile += %w( Flat-UI-Icons-24.ttf )
+Rails.application.config.assets.precompile += %w( Flat-UI-Icons-24.svg )
+Rails.application.config.assets.precompile += %w( checkbox.png )
+Rails.application.config.assets.precompile += %w( radio.png )
+Rails.application.config.assets.precompile += %w( checkbox-2x.png )
