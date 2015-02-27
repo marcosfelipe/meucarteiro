638912739023f5469ecf5dbbc6770b55b28dd4d6
marcosdebian <marcos.felype@hotmail.com>
2015-02-26 23:45:33 -0300

    implantando o devise para autenticação no sys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-
+  #
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  #
+  # protected
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :username
+  #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation) }
+  # end
 
 end
