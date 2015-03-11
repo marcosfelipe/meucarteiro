c48f46f37fa88ac7b933383550c14fac3a971202
Felipe <felipexsouza@icloud.com>
2015-03-10 22:01:11 -0300

    teste ola mundo
    [felipe]

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 20150301223022) do
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "city",       limit: 255
-    t.string   "email",      limit: 255
+    t.string   "name"
+    t.string   "city"
+    t.string   "email"
     t.integer  "phone"
     t.date     "birth"
     t.boolean  "active"
@@ -43,16 +43,16 @@ ActiveRecord::Schema.define(version: 20150301223022) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
